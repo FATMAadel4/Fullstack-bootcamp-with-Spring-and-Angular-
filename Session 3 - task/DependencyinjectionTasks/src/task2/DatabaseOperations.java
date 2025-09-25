b8708003203d9c739222eda422a056100dda31b6
FATMAadel4 <fatmaadelmo@gmail.com>
2025-09-25 18:53:43 +0300

    add tasks

---
+++ Session 3 - task/DependencyinjectionTasks/src/task2/DatabaseOperations.java
@@ -0,0 +1,49 @@
+package task2;
+
+public class DatabaseOperations
+{
+    private String url;
+    private String username;
+    private String password;
+
+    public DatabaseOperations() {
+    }
+    public DatabaseOperations(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public void save(Vehicle vehicle)
+    {
+        System.out.println("---------------connecting to database with properties------------------");
+        System.out.println(url + " " + username + " " + password);
+        System.out.printf("--------------%s saved to database------------------\n" , vehicle.getBrand());
+    }
+}
