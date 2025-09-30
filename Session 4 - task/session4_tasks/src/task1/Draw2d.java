eee5a040b2c40cd77b8a517b566cae9bcc446d80
FATMAadel4 <163375893+FATMAadel4@users.noreply.github.com>
2025-09-30 17:23:42 +0300

    Delete Session 4 - task directory

--- Session 4 - task/session4_tasks/src/task1/Draw2d.java
+++
@@ -1,7 +0,0 @@
-package task1;
-
-public class Draw2d {
-    public void draw(String shapeName) {
-        System.out.println("Drawing " + shapeName + " in 2D");
-    }
-}
--- Session 4 - task/session4_tasks/src/task1/Draw3d.java
+++
@@ -1,7 +0,0 @@
-package task1;
-
-public class Draw3d {
-    public void draw(String shapeName) {
-        System.out.println("Drawing " + shapeName + " in 3D");
-    }
-}
