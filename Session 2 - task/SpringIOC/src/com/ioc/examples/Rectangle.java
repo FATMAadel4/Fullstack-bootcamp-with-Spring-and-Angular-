5449a6bf97329207234195fefed31fa1ba6022fe
FATMAadel4 <fatmaadelmo@gmail.com>
2025-10-03 13:48:29 +0300

    Update applicationContext.xml with properties and Rectangle fields

--- Session 2 - task/SpringIOC/src/com/ioc/examples/Rectangle.java
+++ Session 2 - task/SpringIOC/src/com/ioc/examples/Rectangle.java
@@ -1,13 +1,18 @@
 package com.ioc.examples;
 
 public class Rectangle implements Shape {
-    private double side;
-    public void setSide(double side) {
-        this.side = side;
-    }
-    @Override
-    public double getArea() {
-        return side* side;
+    private int width;
+    private int height;
+
+    // getters & setters
+    public int getWidth() { return width; }
+    public void setWidth(int width) { this.width = width; }
+
+    public int getHeight() { return height; }
+    public void setHeight(int height) { this.height = height; }
+
+    public int getarea() {
+        return width * height;
     }
 }
 
