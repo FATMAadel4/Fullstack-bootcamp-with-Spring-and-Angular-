146f256cb0ae665a93138aa4a81edb43214d6477
FATMAadel4 <fatmaadelmo@gmail.com>
2025-10-11 22:42:13 +0300

    feat: add user registration form | Session 8 - task

---
+++ Session 8 - task/src/main/com/myApp/model/UserModel.java
@@ -0,0 +1,56 @@
+package main.com.myApp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+
+@Component
+
+public class UserModel{
+    private String userName;
+    private String password;
+    private String country;
+    private String programmingLanguage;
+    private String operatingSystem;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+}
