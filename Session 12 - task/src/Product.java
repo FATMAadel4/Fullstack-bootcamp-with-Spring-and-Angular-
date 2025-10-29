87efed65f2ace86b6eeb2ff93adc9894f0dfd0c7
FATMAadel4 <fatmaadelmo@gmail.com>
2025-10-29 14:38:47 +0300

    feat:  apply the same HQL that explained in the session andstore date/time in Hibernate | Session 12 - task

---
+++ Session 12 - task/src/Product.java
@@ -0,0 +1,52 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "products")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int productId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "stock_quantity")
+    private int stockQuantity;
+
+    @Column(name = "manufacturer")
+    private String manufacturer;
+
+    @Column(name = "production_date")
+    private Date productionDate;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", stockQuantity=" + stockQuantity +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", productionDate=" + productionDate +
+                '}';
+    }
+}
